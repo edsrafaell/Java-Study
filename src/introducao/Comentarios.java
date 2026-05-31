bb2c7ab9c8924366f5b1e53c589f38f5da77f540
EdsonRafaelS <eds.rrafael@gmail.com>
2026-05-31 13:19:01 -0300

    Study: Comentários
    // Em linha
    /* * */ Em bloco
    /** * */ Documentação

---
+++ src/introducao/Comentarios.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class Comentarios {
+    public static void main(String[] args){
+        // Comentário em linha
+        /*
+         * Comentário em bloco
+         */
+        /**
+         * Comentário de documentação
+         * @author Rafael
+         * @version 1.0
+         */
+    }
+}
