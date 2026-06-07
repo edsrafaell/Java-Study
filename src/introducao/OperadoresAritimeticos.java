eef6b4f677992e361c1090e81fc271b721ac767e
EdsonRafaelS <eds.rrafael@gmail.com>
2026-06-06 23:12:26 -0300

    Create Operadores Aritiméticos
    Soma, subtração, divisão e multiplicação

---
+++ src/introducao/OperadoresAritimeticos.java
@@ -0,0 +1,20 @@
+package introducao;
+
+public class OperadoresAritimeticos {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 12;
+        
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        int divisao = a / b;
+        int modulo = a % b;
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+        System.out.println("Módulo: " + modulo);
+
+    }
+}
