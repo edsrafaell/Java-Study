27c179bf87d42bd6e2aa7ad1fd7ff42f2c2d120a
EdsonRafaelS <eds.rrafael@gmail.com>
2026-06-10 22:56:00 -0300

    Study Laços Repetições
    For, While e  Do While

---
+++ src/estruturaRepeticoes/lacoWhile.java
@@ -0,0 +1,11 @@
+package estruturaRepeticoes;
+
+public class lacoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 10){
+            System.out.println(i);
+            i++;;
+        }
+    }
+}
